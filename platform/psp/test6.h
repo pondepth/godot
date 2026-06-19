c678b25704ff2ead7fa9b0dd8de0bfc7bc4418ea
pon <73057608+pondepth@users.noreply.github.com>
2026-06-19 22:06:38 +0300

    test

---
+++ platform/psp/test6.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "core/variant/type_info.h"
+#ifdef PSP_ENABLED
+template <>
+struct GetTypeInfo<Math::int_alt_t> {
+static const Variant::Type VARIANT_TYPE = Variant::INT;
+static const GodotTypeInfo::Metadata METADATA = GodotTypeInfo::METADATA_INT_IS_INT32;
+static inline PropertyInfo get_class_info() { return PropertyInfo(VARIANT_TYPE, String()); }
+};
+#endif
